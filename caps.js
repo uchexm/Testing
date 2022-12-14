a59bed8842018d106d8cc34b45d4a7d806a3ca89
uchexm <109084069+uchexm@users.noreply.github.com>
2022-12-14 08:06:14 +0200

    Add files to test

---
+++ caps.js
@@ -0,0 +1,9 @@
+const capitalize = (str) => {
+  if (Object.prototype.toString.call(str) !== "[object String]") {
+    return;
+  } else {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+};
+
+module.exports = capitalize;
